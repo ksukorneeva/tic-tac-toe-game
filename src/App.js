c3178722961d4f82813182c14bb6727b2e10ed50
Ксения Корнеева <user@MacBook-Pro-User.local>
2022-07-18 22:12:43 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [player, setPlayer] = useState(null);
+    const [winner, setWinner] = useState(null);
+
+    const clickHandler = (index) => {
+        if (board[index] !== null || winner !== null) return;
+        const copyBoard = [...board];
+        copyBoard[index] = player;
+        setBoard(copyBoard);
+        setPlayer(player === 'X' ? 'O' : 'X');
+    };
+
+    const checkWinner = useCallback(() => {
+        const listWinner = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < listWinner.length; i++) {
+            const [a, b, c] = listWinner[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c])
+                setWinner(board[a]);
+        }
+        const draw = board.every((elem) => elem !== null);
+        draw && setWinner('дружба');
+    }, [setWinner, board]);
+
+    const resetGame = () => {
+        setBoard(Array(9).fill(null));
+        setWinner(null);
+        setPlayer(null);
+    };
+
+    useEffect(() => {
+        checkWinner();
+    }, [checkWinner]);
+
+    if (!player) {
+        return (
+            <>
+                <h2>Чем хотите играть?</h2>
+                <div className='actions'>
+                    <button onClick={() => setPlayer('X')}>Крестик</button>
+                    <button onClick={() => setPlayer('O')}>Нолик</button>
+                </div>
+            </>
+        );
+    }
+    return (
+        <>
+            {' '}
+            {winner ? (
+                <>
+                    <h1>Победил(a): {winner}</h1>
+                    <button onClick={resetGame}>Начать заново</button>
+                </>
+            ) : (
+                <h1>Сейчас ходит: {player}</h1>
+            )}
+            <div className='field'>
+                {board.map((item, index) => (
+                    <div
+                        className='field-grid'
+                        key={index}
+                        onClick={() => clickHandler(index)}
+                    >
+                        {item}
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default App;
